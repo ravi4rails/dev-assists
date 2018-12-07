28575717640c0320ddedfe6fc7d55511f8eabdfb
ravi4rails <ravionrails@gmail.com>
2018-12-07 21:58:07 +0530

    working on styles

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,13 @@
 class Developer < ApplicationRecord
+
+  validates :first_name, :last_name, :contact_number, presence: true
+
   has_many :developer_skills
   has_many :skills, through: :developer_skills
   mount_uploader :profile_photo, ImageUploader
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 end
