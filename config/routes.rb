7a516334b6d0e4cdc3b202a878669fa9f881b92c
ravi4rails <ravionrails@gmail.com>
2018-12-07 16:20:46 +0530

    added devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :admins, path: 'admins', controllers: {
+    sessions:             'admins/sessions',
+    passwords:            'admins/passwords',
+    registrations:        'admins/registrations',
+    confirmations:        'admins/confirmations',
+    ominiauth_callbacks:  'admins/ominiauth_callbacks',
+    unlocks:              'admins/unlocks'
+  }
+  
   namespace :admin do
     resources :developers
     resources :skills
