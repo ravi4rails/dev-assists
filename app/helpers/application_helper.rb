d49c653141deda3a5949ad8b255b7eb36601f1e0
ravi4rails <ravionrails@gmail.com>
2018-12-12 18:21:05 +0530

    added developers dropdown in project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def developer_list
+    Developer.all.map {|developer| [developer.first_name, developer.id] }
+  end
 end
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -6,7 +6,7 @@ class Developer < ApplicationRecord
 
   validates :first_name, :last_name, :contact_number, presence: true
   
-  has_many :projects
+  has_many :projects, dependent: :destroy
   has_many :developer_skills
   has_many :skills, through: :developer_skills
   mount_uploader :profile_photo, ImageUploader
